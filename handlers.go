0385d4975309f62e34ab55dabf23940d1c87a6ad
Eva Harris <mail@gastrodon.io>
2021-07-15 17:49:50 -0400

    register_handlers

---
+++ handlers.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gastrodon/groudon/v2"
+
+	"os"
+)
+
+const (
+	uuid_regex = `[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}`
+)
+
+var (
+	prefix = os.Getenv("PATH_PREFIX")
+
+	routeId   = "^" + prefix + "/id/" + uuid_regex + "/?$"
+	routeNick = "^" + prefix + "/nick/" + uuid_regex + "/?$"
+)
+
+func register_handlers() {
+	groudon.AddHandler("GET", routeId, getUserID)
+	groudon.AddHandler("GET", routeNick, getUserNick)
+}
