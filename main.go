f2da97248a3d52186835007b0ca2949fcadb148c
Eva <mail@gastrodon.io>
2021-11-16 13:38:33 -0500

    Add main.go

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/brane-app/database-library"
+	"github.com/gastrodon/groudon/v2"
+
+	"log"
+	"net/http"
+	"os"
+)
+
+func health_check(_ *http.Request) (int, map[string]interface{}, error) {
+	return 204, nil, database.Health()
+}
+
+func main() {
+	database.Connect(os.Getenv("DATABASE_CONNECTION"))
+
+	groudon.AddHandler("GET", "^/health/?$", health_check)
+	register_handlers()
+
+	prefix := os.Getenv("PATH_PREFIX")
+	log.Printf("Routing with a prefix %s\n", prefix)
+
+	http.Handle("/", http.HandlerFunc(groudon.Route))
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
